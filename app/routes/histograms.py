a9dbaa11cadd445867092b8bb52c151b53ab84e9
Pierangelo Loi <p.loi5@studenti.unica.it>
2022-03-03 18:51:02 +0100

    Added files:

---
+++ app/routes/histograms.py
@@ -0,0 +1,30 @@
+import redis
+from flask import render_template
+from rq import Connection, Queue
+from rq.job import Job
+
+from app import app
+from app.forms.histogram_form import HistogramForm
+from config import Configuration
+
+config = Configuration()
+
+
+@app.route('/histograms', methods=['GET', 'POST'])
+def histograms():
+    """API for:
+
+    qualcosa
+
+    """
+    form = HistogramForm()
+
+    if form.validate_on_submit():  # POST
+        image_id = form.image.data
+        image = form.image
+
+        return render_template("histogram_output_queue.html", image_id=image_id, inImg=image)
+
+    # otherwise, it is a get request and should return the
+    # image and model selector
+    return render_template('histogram_select.html', form=form)
---
+++ app/static/histogram.js
@@ -0,0 +1,156 @@
+/**
+ * Simple long polling client based on JQuery
+ * https://github.com/sigilioso/long_polling_example/blob/master/static/poll.js
+ */
+
+/**
+ * Request an update to the server and once it has answered, then update
+ * the content and request again.
+ * The server is supposed to response when a change has been made on data.
+ */
+function update(image_id) {
+    $.ajax({
+        url: `/histograms/${image_id}`,
+        success: function (data) {
+            console.log(data)
+            switch (data['task_status']) {
+                case "finished":
+                    $('#spinner').hide();
+                    $('#waitText').text("");
+                    makeGraph(image_id);
+                    break;
+                case "started":
+                    $('#waitText').text("Job started...");
+                    $('#spinner').show();
+                    setTimeout(function () {
+                        update(image_id);
+                    }, 1000);
+                    break;
+                case "queued":
+                    $('#waitText').text("Please wait ...");
+                    $('#spinner').show();
+                    setTimeout(function () {
+                        update(image_id);
+                    }, 1000);
+                    break;
+            }
+
+        }
+    });
+}
+
+
+$(document).ready(function () {
+    var scripts = document.getElementById('readImg');
+    var image_id = scripts.getAttribute('image_id');
+    update(image_id);
+});
+
+function makeGraph(image_id) {
+  processImage(getImageData(image_id))
+}
+
+function processImage(inImg) {
+  const width = inImg.width;
+  const height = inImg.height;
+  const src = new Uint32Array(inImg.data.buffer);
+  const isValueHistogram = $("#typeValue").prop('checked');
+
+  let histBrightness = (new Array(256)).fill(0);
+  let histR = (new Array(256)).fill(0);
+  let histG = (new Array(256)).fill(0);
+  let histB = (new Array(256)).fill(0);
+  for (let i = 0; i < src.length; i++) {
+    let r = src[i] & 0xFF;
+    let g = (src[i] >> 8) & 0xFF;
+    let b = (src[i] >> 16) & 0xFF;
+    histBrightness[r]++;
+    histBrightness[g]++;
+    histBrightness[b]++;
+    histR[r]++;
+    histG[g]++;
+    histB[b]++;
+  }
+
+  let maxBrightness = 0;
+  if (isValueHistogram) {
+    for (let i = 1; i < 256; i++) {
+      if (maxBrightness < histBrightness[i]) {
+        maxBrightness = histBrightness[i]
+      }
+    }
+  } else {
+    for (let i = 0; i < 256; i++) {
+      if (maxBrightness < histR[i]) {
+        maxBrightness = histR[i]
+      } else if (maxBrightness < histG[i]) {
+        maxBrightness = histG[i]
+      } else if (maxBrightness < histB[i]) {
+        maxBrightness = histB[i]
+      }
+    }
+  }
+
+  const canvas = document.getElementById('canvasHistogram');
+  const ctx = canvas.getContext('2d');
+  let guideHeight = 8;
+  let startY = (canvas.height - guideHeight);
+  let dx = canvas.width / 256;
+  let dy = startY / maxBrightness;
+  ctx.lineWidth = dx;
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < 256; i++) {
+    let x = i * dx;
+    if (isValueHistogram) {
+      // Value
+      ctx.strokeStyle = "#000000";
+      ctx.beginPath();
+      ctx.moveTo(x, startY);
+      ctx.lineTo(x, startY - histBrightness[i] * dy);
+      ctx.closePath();
+      ctx.stroke();
+    } else {
+      // Red
+      ctx.strokeStyle = "rgba(220,0,0,0.5)";
+      ctx.beginPath();
+      ctx.moveTo(x, startY);
+      ctx.lineTo(x, startY - histR[i] * dy);
+      ctx.closePath();
+      ctx.stroke();
+      // Green
+      ctx.strokeStyle = "rgba(0,210,0,0.5)";
+      ctx.beginPath();
+      ctx.moveTo(x, startY);
+      ctx.lineTo(x, startY - histG[i] * dy);
+      ctx.closePath();
+      ctx.stroke();
+      // Blue
+      ctx.strokeStyle = "rgba(0,0,255,0.5)";
+      ctx.beginPath();
+      ctx.moveTo(x, startY);
+      ctx.lineTo(x, startY - histB[i] * dy);
+      ctx.closePath();
+      ctx.stroke();
+    }
+    // Guide
+    ctx.strokeStyle = 'rgb(' + i + ', ' + i + ', ' + i + ')';
+    ctx.beginPath();
+    ctx.moveTo(x, startY);
+    ctx.lineTo(x, canvas.height);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+function getImageData(el) {
+  const canvas = document.getElementById('histogramOutput');
+  const context = canvas.getContext('2d');
+  const img = document.getElementById(el);
+  canvas.width = img.width;
+  canvas.height = img.height;
+  context.drawImage(img, 0, 0);
+  return context.getImageData(0, 0, img.width, img.height);
+}
+
